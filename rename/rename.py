1f61bb81c5233db91e8bd770962dc6d0b1e131b1
Mark Vandevoorde <mtv@bytegain.com>
2018-08-16 15:29:27 -0700

    Use cwd as root of changes (instead of one level up from cwd)

--- rename/rename.py
+++ rename/rename.py
@@ -37,9 +37,11 @@ def __valid_word(word, old_word, filetypes):
 
 
 # do
-replace(dirname(abspath(".")), "SEGMENT", "BYTEGAIN", [".h", ".m"], abspath("./changelog.txt"))
-replace(dirname(abspath(".")), "SEG", "ByteGain", [".h", ".m"], abspath("./changelog.txt"))
+# replace(".", "SEGMENT", "BYTEGAIN", [".h", ".m"], abspath("./changelog.txt"))
+# replace(".", "SEG", "ByteGain", [".h", ".m"], abspath("./changelog.txt"))
 # Undo
+replace(".", "BYTEGAIN", "SEGMENT", [".h", ".m"], abspath("./changelog.txt"))
+replace(".", "ByteGain", "SEG", [".h", ".m"], abspath("./changelog.txt"))
 # replace(dirname(abspath(".")), "BYTEGAIN", "SEGMENT", [".h", ".m"], abspath("./changelog.txt"))
 # replace(dirname(abspath(".")), "ByteGain", "SEG", [".h", ".m"], abspath("./changelog.txt"))
 

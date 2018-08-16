5edabf36ddc9c94a4a27e46ece3432e8182b3b0a
Mark Vandevoorde <mtv@bytegain.com>
2018-08-16 15:08:46 -0700

    Use "with" statements for file operations

--- rename/rename.py
+++ rename/rename.py
@@ -6,30 +6,26 @@ from os.path import dirname, join, abspath, splitext
 
 
 def replace(dirpath, old_word, new_word, filetypes, changelog_path):
-    changelog = open(changelog_path, "a")
-    dirdata = walk(dirpath)
+    with open(changelog_path, "a") as changelog:
+        dirdata = walk(dirpath)
 
-    for (top_dir, subdirs, file_names) in dirdata.__iter__():
-        for file_name in file_names:
-            if splitext(file_name)[1] in filetypes:
-                filepath = join(top_dir, file_name)
-                changelog.write(filepath + "\n")
+        for (top_dir, subdirs, file_names) in dirdata.__iter__():
+            for file_name in file_names:
+                if splitext(file_name)[1] in filetypes:
+                    filepath = join(top_dir, file_name)
+                    changelog.write(filepath + "\n")
 
-                f = open(filepath, "r")
-                lines = []
-                for line in f:
-                    if old_word in line and not line.startswith("#import"):
-                        lines.append(line.replace(old_word, new_word))
-                        changelog.write("\tReplaced: " + line)
-                        changelog.write("\tWith: " + line.replace(old_word, new_word))
-                    else:
-                        lines.append(line)
-
-                f.close()
-                f = open(join(top_dir, file_name), "w")
-                f.writelines(lines)
-                f.close()
-    changelog.close()
+                    with open(filepath, "r") as f:
+                        lines = []
+                        for line in f:
+                            if old_word in line and not line.startswith("#import"):
+                                lines.append(line.replace(old_word, new_word))
+                                changelog.write("\tReplaced: " + line)
+                                changelog.write("\tWith: " + line.replace(old_word, new_word))
+                            else:
+                                lines.append(line)
+                    with open(join(top_dir, file_name), "w") as f:
+                        f.writelines(lines)
 
 
 def __valid_word(word, old_word, filetypes):
